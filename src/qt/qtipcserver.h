77d3b54a25454b6ae689bbe317164c2c51edf898
Rohan Kumar <rohanjha1992@gmail.com>
2017-12-25 16:33:24 +0530

    ECE ecoinonline source file

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ecoinonline-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ecoinonlineURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
